559db5a9169d8f068c709c2a87986217ed563da9
pallavi1812 <upallavi1812@gmail.com>
2021-02-17 12:11:01 +0530

    expense-tracker

---
+++ src/context/context.js
@@ -0,0 +1,27 @@
+import React, { useReducer, createContext } from 'react';
+import contextReducer from './contextReducer';
+const initialState = [];
+
+export const ExpenseTrackerContext = createContext(initialState);
+
+export const Provider = ({ children }) => {
+
+    // const [state, dispatch] = useReducer(reducer, initialState)
+    const [transactions, dispatch] = useReducer(contextReducer, initialState);
+    //Action Creators
+    const deleteTransaction = (id) => dispatch({ type: 'DELETE_TRANSACTION', payload: id });
+    const addTransaction = (transaction) => dispatch({ type: 'ADD_TRANSACTION', payload: transaction });
+
+    const balance = transactions.reduce((acc, currVal) => (currVal.type === 'Expense' ? acc - currVal.amount : acc + currVal.amount), 0);
+
+    return (
+        <ExpenseTrackerContext.Provider value={{
+            deleteTransaction,  //deleteTransaction: deleteTransaction
+            addTransaction,
+            transactions,
+            balance
+        }}>
+            {children}
+        </ExpenseTrackerContext.Provider>
+    )
+}
